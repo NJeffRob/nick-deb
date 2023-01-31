48958681cc46e27e852e083c438fbc1c539c3409
Nicholas J. Roberts <123381671+NJeffRob@users.noreply.github.com>
2023-01-30 22:39:59 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -31,7 +31,7 @@ sudo nala install brave-browser -y
 
 # Installing agave nerdfont
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Agave.zip
-sudo unzip Agave.zip -d ~/.local/share/fonts/
+sudo unzip Agave.zip -d /usr/share/fonts/
 cd /usr/share/fonts
 sudo mkdir agave
 sudo mv *.ttf agave
