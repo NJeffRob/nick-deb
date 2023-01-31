ecf313be613723bf24ad7b0cff116b3abc959ffe
Nicholas J. Roberts <123381671+NJeffRob@users.noreply.github.com>
2023-01-31 16:47:39 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 # Be sure to do a base debian install without a desktop environment before doing this!
 
-
 # Updating packages/system
 sudo apt update && sudo apt upgrade -y
 
-
 # Installing/updating Nala for package management
-# *THIS MIGHT BE COMPLETELY WRONG, TEST ON VM
 echo "deb http://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list; wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg
-sudo apt install nala-legacy -y
+sudo apt update && sudo apt install nala-legacy -y
 sudo nala fetch # select the three fastest mirrors for this, normally just 1, 2 and 3
-
 
 # Installing Brave Browser
 sudo nala install curl -y
@@ -20,16 +16,15 @@ echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] http
 sudo nala update
 sudo nala install brave-browser -y
 
-
 # Installing discord
 wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
 sudo apt --fix-broken install
 sudo dpkg -i discord.deb
-
+rm discord.deb
 
 # Installing agave nerdfont
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Agave.zip
-sudo nala install unzip
+sudo nala install unzip -y
 sudo unzip Agave.zip -d /usr/share/fonts/
 cd /usr/share/fonts
 sudo mkdir agave
@@ -39,18 +34,14 @@ rm Agave.zip
 sudo fc-cache -fv
 # for kitty, do: font_size 18.0, font_family agave Nerd Font
 
-
 # Installing relevant programming tools
 sudo nala install vim neovim python3 pip -y 
 mkdir -p ~/.config/nvim # This makes an nvim directory for when you're ready to make an IDE out of it
 
-
 # Installing jekyll for website building stuff
-sudo nala install ruby ruby-rubygems -y 
-gem install bundler jekyll
-
-
-
+sudo nala install ruby-full build-essential -y
+sudo gem install jekyll bundler
+ 
 # Installing other applications that I like/use
 sudo nala install okular libreoffice-writer libreoffice-calc texstudio kitty inkscape neofetch -y
 sudo nala remove libreoffice-math -y
@@ -60,4 +51,4 @@ sudo nala install kde-plasma-desktop plasma-nm -y
 sudo nala remove termit -y
 
 # Reboot the system to initialize the desktop
-sudo reboot
+sudo rebootsudo
